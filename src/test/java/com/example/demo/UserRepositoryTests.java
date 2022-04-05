5ca386a5d4c3080d5fee1c6917c598638860f3cc
vicky <mvivekanandareddy@accelaero.com>
2022-04-05 18:00:02 +0530

    1st commit

---
+++ src/test/java/com/example/demo/UserRepositoryTests.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+
+public class UserRepositoryTests {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	@Autowired
+	private TestEntityManager entityManager;
+	
+	@Test
+	public void testCreateUser() {
+		User user = new User();
+		user.setEmail("111801024@smail.iitpkd.ac.in");
+		user.setPassword("vicky2331");
+		user.setFirstName("Vicky");
+		user.setLastName("Reddy");
+		
+		User savedUser = repo.save(user);
+		
+		User existUser = entityManager.find(User.class, savedUser.getId());
+		
+		assertThat(existUser.getEmail()).isEqualTo(user.getEmail());
+	}
+	
+}
